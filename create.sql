81e181599d51336293c6fa91fd4f31e1857d22dd
kenny <baoboo233@gmail.com>
2021-04-30 03:36:51 -0400

    final update

--- create.sql
+++ create.sql
@@ -6,25 +6,25 @@ CREATE TABLE IF NOT EXISTS order_track_user(
     otu_role SMALLINT NOT NULL,
     otu_is_active BOOLEAN NOT NULL,
     otu_change_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    ot_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    otu_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(otu_id)
 )ENGINE = MYISAM CHARACTER SET utf8mb4;
 
 CREATE TABLE IF NOT EXISTS order_track_order(
     oto_id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL,
-    oto_customer VARCHAR(128) NOT NULL,
+    oto_customer VARCHAR(32) NOT NULL,
     oto_weight DECIMAL(10,2) NOT NULL,
     oto_total_price DECIMAL(10,2) NOT NULL,
     oto_trans_price DECIMAL(10,2) NOT NULL,
     oto_package_price DECIMAL(10,2) NOT NULL,
     oto_status SMALLINT NOT NULL,
     oto_order_time TIMESTAMP NOT NULL,
-    oto_finish_time TIMESTAMP NOT NULL,
+    oto_finish_time TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
     oto_change_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    oto_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    oto_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(oto_id),
     FOREIGN KEY(oto_customer)
-        REFERENCES order_user(otu_name)
+        REFERENCES order_user(otu_user_name)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 )ENGINE = MYISAM CHARACTER SET utf8mb4;
@@ -35,12 +35,13 @@ CREATE TABLE IF NOT EXISTS order_track_order_detail(
     otod_product_id INT NOT NULL,
     otod_package_id INT NOT NULL,
     otod_package_count INT NOT NULL,
-    otod_total_price DECIMAL(10,2) NOT NULL,
+    otod_product_price DECIMAL(10,2) NOT NULL,
     otod_trans_price DECIMAL(10,2) NOT NULL,
     otod_package_price DECIMAL(10,2) NOT NULL,
+    otod_weight DECIMAL(10,2) NOT NULL,
     otod_is_finish BOOLEAN NOT NULL,
     otod_change_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    otod_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    otod_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(otod_id),
     FOREIGN KEY(otod_order_id)
         REFERENCES order_track_order(oto_id)
@@ -64,7 +65,7 @@ CREATE TABLE IF NOT EXISTS order_track_package(
     otp_stock INT NOT NULL,
     otp_is_active BOOLEAN NOT NULL,
     otp_change_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    otp_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    otp_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(otp_id)
 )ENGINE = MYISAM CHARACTER SET utf8mb4;
 
@@ -74,32 +75,41 @@ CREATE TABLE IF NOT EXISTS order_track_product(
     otpr_price DECIMAL(10,2) NOT NULL,
     otpr_is_active BOOLEAN NOT NULL,
     otpr_change_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    otpr_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    otpr_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(otpr_id)
+)ENGINE = MYISAM CHARACTER SET utf8mb4;
+
+CREATE TABLE IF NOT EXISTS order_track_car(
+    otc_id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL,
+    otc_plate VARCHAR(8) NOT NULL,
+    otc_weight DECIMAL(10,2) NOT NULL,
+    otc_price DECIMAL(10,2) NOT NULL,
+    otc_type TINYINT NOT NULL,
+    otc_is_active BOOLEAN NOT NULL,
+    otc_change_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    otc_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 )ENGINE = MYISAM CHARACTER SET utf8mb4;
 
 CREATE TABLE IF NOT EXISTS order_track_transport(
     ott_id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL,
-    ott_plate VARCHAR(8) NOT NULL,
-    ott_order_id INT NOT NULL,
+    ott_car_id INT NOT NULL,
     ott_order_detail_id INT NOT NULL,
-    ott_count INT NOT NULL,
-    ott_weight DECIMAL(10,2) NOT NULL,
-    ott_price DECIMAL(10,2) NOT NULL,
-    ott_type TINYINT NOT NULL,
-    ott_isactive BOOLEAN NOT NULL,
     ott_change_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    ott_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    ott_create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(ott_id),
-    FOREIGN KEY(ott_order_id, ott_order_detail_id)
-        REFERENCES order_track_order_detail(otod_order_id, otod_id)
+    FOREIGN KEY(ott_car_id)
+        REFERENCES order_track_car(otc_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    FOREIGN KEY(ott_order_detail_id)
+        REFERENCES order_track_order_detail(otod_id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 )ENGINE = MYISAM CHARACTER SET utf8mb4;
 
 CREATE TABLE IF NOT EXISTS order_track_work_record(
     otwr_id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL,
-    otwr_name VARCHAR(32) NOT NULL UNIQUE,
+    otwr_name VARCHAR(32) NOT NULL,
     otwr_order_detail_id INT NOT NULL,
     otwr_type SMALLINT NOT NULL,
     otwr_workload INT NOT NULL,
